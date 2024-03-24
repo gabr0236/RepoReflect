a1cc1024e1aa2aea8508076bf6228ccd2ad7795d
Gabriel Haugbøl <gabriel.haugbol@gmail.com>
2024-03-24 16:14:58 +0100

    Reformat file

--- RepoReflect.App/Services/ReflectService.cs
+++ RepoReflect.App/Services/ReflectService.cs
@@ -1,18 +1,19 @@
+using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using CliWrap;
 
 namespace Console.App.Services;
 
 public class ReflectService
 {
-
     private readonly IHttpClientFactory _httpClientFactory;
 
     public ReflectService(IHttpClientFactory httpClientFactory)
     {
         _httpClientFactory = httpClientFactory;
     }
-   
+
     public async Task GetHistory(string privateKey, string projectId, string author)
     {
         var client = _httpClientFactory.CreateClient();
@@ -20,19 +21,21 @@ public class ReflectService
         client.DefaultRequestHeaders.Add("PRIVATE-TOKEN", privateKey);
 
         var commits = new List<GitLabCommit>();
-        
+
         var page = 1;
         bool hasMorePages = true;
-        
+
         while (hasMorePages)
         {
-            var response = await client.GetAsync($"https://gitlab.com/api/v4/projects/{projectId}/repository/commits?page={page}&per_page=99&author={author}");
-            
+            var response =
+                await client.GetAsync(
+                    $"https://gitlab.com/api/v4/projects/{projectId}/repository/commits?page={page}&per_page=99&author={author}");
+
             if (!response.IsSuccessStatusCode)
             {
                 throw new Exception($"Error while requesting history");
             }
-            
+
             var content = await response.Content.ReadAsStringAsync();
             var newCommits = JsonSerializer.Deserialize<List<GitLabCommit>>(content);
             commits.AddRange(newCommits!);
@@ -40,7 +43,7 @@ public class ReflectService
             page++;
             if (newCommits!.Count == 0) hasMorePages = false;
         }
-        
+
         System.Console.WriteLine($"{author} has this many commits: {commits.Count}");
         System.Console.WriteLine("Exiting GetHistory");
     }
@@ -58,6 +61,8 @@ public class ReflectService
     private record GitLabCommit(
         [property: JsonPropertyName("id")] string Id,
         [property: JsonPropertyName("title")] string Title, //TODO: delete
-        [property: JsonPropertyName("created_at")] DateTime CreatedAt,
-        [property: JsonPropertyName("message")] string Message); //TODO: delete
+        [property: JsonPropertyName("created_at")]
+        DateTime CreatedAt,
+        [property: JsonPropertyName("message")]
+        string Message); //TODO: delete
 }

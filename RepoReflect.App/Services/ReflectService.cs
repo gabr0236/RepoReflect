1f7c4e9768308d1787a10d1781bc78c0e856af10
Gabriel Haugbøl <gabriel.haugbol@gmail.com>
2024-03-28 00:48:29 +0100

    Refactor ReflectService.cs to isolate logic

--- RepoReflect.App/Services/ReflectService.cs
+++ RepoReflect.App/Services/ReflectService.cs
@@ -1,4 +1,5 @@
 using CliWrap;
+using Console.App.Types;
 
 namespace Console.App.Services;
 
@@ -11,64 +12,31 @@ public class ReflectService
         _logger = logger;
         _gitLabService = gitLabService;
     }
-
-    public async Task ReflectCommitsToExistingRepo(
-        string privateKey,
-        string projectId,
-        string author,
-        string pathToRepo,
-        string customCommitMessage = "Committed to Master")
-    {
-        var commits = await _gitLabService.GetGitLabCommits(privateKey, projectId, author);
-
-        System.Console.WriteLine("Reflecting...");
-
-        for (var i = 0; i < commits.Count; i++)
-        {
-            await ReflectCommit(commits[i].CreatedAt, customCommitMessage, commits[i].Id, pathToRepo);
-
-            var progress = i * 100 / commits.Count;
-            System.Console.Write("\r{0}% ", progress);
-        }
-        System.Console.WriteLine("\r100%");
-        
-        System.Console.WriteLine($"Successfully added {commits.Count} Contributions to {pathToRepo}");
-    }
     
-    public async Task ReflectEventsToExistingRepo(string privateKey, string projectId, string author, string pathToRepo)
-    {
-
-        var events = await _gitLabService.GetGitLabEvents(privateKey, projectId, author);
-
-        System.Console.WriteLine("Reflecting...");
-        
-        for (var i = 0; i < events.Count; i++)
-        {
-            await ReflectEvent(events[i].CreatedAt, Helpers.GetEventName(events[i], _logger), events[i].Id, pathToRepo);
-            
-            var progress = i * 100 / events.Count;
-            System.Console.Write("\r{0}% ", progress);
-        }
-        System.Console.WriteLine("\r100%");
-        
-        System.Console.WriteLine($"Successfully added {events.Count} Contributions to {pathToRepo}");
-    }
-    
-    private async Task ReflectCommit(DateTime createdAt, string message, string originalSha, string pathToRepo)
+    private async Task ReflectContribution(Contribution contribution,string pathToRepo)
     {
         await Cli.Wrap("git")
-            .WithArguments($"commit --allow-empty --date \"{createdAt}\" -m \"{message}\" -m \"{originalSha}\"") 
+            .WithArguments($"commit --allow-empty --date \"{contribution.CreatedAt}\" -m \"{contribution.Message}\" -m \"{contribution.Id}\"") 
             .WithWorkingDirectory(pathToRepo)
             .WithValidation(CommandResultValidation.None) // For some reason "git commit --allow-empty returns a non-zero exit code"
             .ExecuteAsync();
     }
-    
-    private async Task ReflectEvent(DateTime createdAt, string message, long id, string pathToRepo)
+
+    public async Task ReflectContributionsToExistingRepo(
+        List<Contribution> contributions,
+        string pathToRepo)
     {
-        await Cli.Wrap("git")
-            .WithArguments($"commit --allow-empty --date \"{createdAt}\" -m \"{message}\" -m \"{id}\"") 
-            .WithWorkingDirectory(pathToRepo)
-            .WithValidation(CommandResultValidation.None) // For some reason "git commit --allow-empty returns a non-zero exit code"
-            .ExecuteAsync();
+        System.Console.WriteLine("Reflecting...");
+
+        for (var i = 0; i < contributions.Count; i++)
+        {
+            await ReflectContribution(contributions[i], pathToRepo);
+
+            var progress = i * 100 / contributions.Count;
+            System.Console.Write("\r{0}% ", progress);
+        }
+        System.Console.WriteLine("\r100%");
+        
+        System.Console.WriteLine($"Successfully added {contributions.Count} Contributions to {pathToRepo}");
     }
 }

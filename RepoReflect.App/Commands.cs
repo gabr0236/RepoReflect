217ae7df20e530e8cd8e8e3c1fbd6612aa3c1de9
Gabriel Haugbøl <gabriel.haugbol@gmail.com>
2024-03-28 00:49:08 +0100

    Refactor Commands.cs to match new isolation of logic refactors

--- RepoReflect.App/Commands.cs
+++ RepoReflect.App/Commands.cs
@@ -7,25 +7,35 @@ public class Commands
 {
     private readonly ReflectService _reflectService;
     private readonly RepositoryService _repositoryService;
+    private readonly GitLabService _gitLabService;
 
-    public Commands(ReflectService reflectService, RepositoryService repositoryService)
+    public Commands(ReflectService reflectService, RepositoryService repositoryService, GitLabService gitLabService)
     {
         _reflectService = reflectService;
         _repositoryService = repositoryService;
+        _gitLabService = gitLabService;
     }
 
     //Author can be either author email or name
     [Command("reflect-commits")]
     public Task ReflectCommitsToExistingRepo(string privateKey, string projectId,string author, string repoPath, [Option] string customMessage)
     {
-        return _reflectService.ReflectCommitsToExistingRepo(privateKey, projectId, author, repoPath, customMessage);
+        var commits = await _gitLabService.GetGitLabCommits(privateKey, projectId, author);
+        
+        //TODO: to contributions
+        
+        return _reflectService.ReflectContributionsToExistingRepo(commits, repoPath);
     }
     
     //Author can be either author email or name
     [Command("reflect-events")]
     public Task ReflectEventsToExistingRepo(string privateKey, string projectId,string author, string repoPath)
     {
-        return _reflectService.ReflectEventsToExistingRepo(privateKey, projectId, author, repoPath);
+        var events = await _gitLabService.GetGitLabEvents(privateKey, projectId, author);
+        
+        //TODO: map to contributions
+        
+        return _reflectService.ReflectContributionsToExistingRepo(events, repoPath);
     }
 
     [Command("create")]
